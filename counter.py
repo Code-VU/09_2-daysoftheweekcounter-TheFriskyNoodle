f9f7ffc8ca3406a05ae1c9562fbbcd7f9b31b3ec
Tom Brentlinger <Tom.Brentlinger@vu.com>
2023-04-10 16:28:12 -0500

    try2

--- counter.py
+++ counter.py
@@ -15,7 +15,8 @@ def countDayOfTheWeek():
                     counts[day] = counts[day] +1
            
     sorted_counts=sorted(counts.items(), key=lambda x:x[1], reverse=True)
-    print(sorted_counts)
+    converted_counts=dict(sorted_counts)
+    print(converted_counts)
 
     
 
