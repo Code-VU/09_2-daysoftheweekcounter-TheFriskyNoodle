3caa0c2e1718bce34c8d6d6220b08ba6eeea8de9
Tom Brentlinger <Tom.Brentlinger@vu.com>
2023-04-10 16:24:04 -0500

    Try

--- counter.py
+++ counter.py
@@ -1,6 +1,25 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    handle = open(file_name)
+    counts = dict()
+    days = list()
+    for line in handle:
+        if line.startswith("From"):
+            if len(line.split()) > 2:
+                words=line.split()
+                day=words[2]
+                if day not in counts:  
+                    counts[day] = 1
+                else:
+                    counts[day] = counts[day] +1
+           
+    sorted_counts=sorted(counts.items(), key=lambda x:x[1], reverse=True)
+    print(sorted_counts)
+
+    
+
+    
 
 
 
